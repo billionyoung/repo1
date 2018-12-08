a411f38dcdae2e57119d89ff83dc4823e69b7677
young <young@123.com>
2018-12-08 20:48:48 +0800

    second

---
+++ mybatis_generation/src/main/java/young/Test.java
@@ -0,0 +1,9 @@
+package young;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello" + 1);
+        System.out.println("Hello" + 2);
+        System.out.println("Hello" + 3);
+    }
+}
